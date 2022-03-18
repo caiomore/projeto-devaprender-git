947f932adbbcc441e0df2eef8eeb092b4701a057
Caio Moreira <caio.workprofile@gmail.com>
2022-03-18 10:54:45 -0300

    commit inicial

---
+++ exemplo02 (1).py
@@ -0,0 +1,6 @@
+#exemplo 02 - entrada e saída
+print("Seja bem-vindo a programação")
+nome = input("Digite o seu nome")
+idade = input("Boa noite " + nome + " qual é a sua idade?")
+
+print("A sua idade é:",idade, "\nO seu nome é: ", nome)
